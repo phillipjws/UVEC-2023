2901e1cf1e5cf2b81fc043012141f75ce160ae9a
mholung <116320008+mholung@users.noreply.github.com>
2023-11-26 15:12:57 -0800

    Create do_not_miss_final.py

---
+++ do_not_miss_final.py
@@ -0,0 +1,60 @@
+# THIS IS MAIN
+import pygame
+import start_cc
+import pygame.freetype
+
+frame = 50
+clock = pygame.time.Clock()
+
+
+class DoNotMissFinal():
+
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((600, 600))
+        pygame.display.set_caption("Don't let Thunder miss the final!")
+        self.clock = pygame.time.Clock()
+        self.running_scene = start_cc.starting_scene()
+
+    def control(self, event, press):
+        x_out = event.type == pygame.QUIT
+        quit = press[pygame.K_q]
+
+        # if anyone click on the cross
+        # button or press the 'q' button
+        # it will quit the window
+        return x_out or (quit)
+
+    def run(self):
+        scene_counter = 0
+
+        while self.running_scene is not None:
+            eve = []
+            press = pygame.key.get_pressed()
+            for event in pygame.event.get():
+                if self.control(event, press):
+                    self.running_scene.terminate()
+                else:
+                    eve.append(event)
+
+            # Manage scene
+            self.running_scene.process_input(eve, press)
+            self.running_scene.update()
+
+            # dont move it as first we need to update then render
+            self.running_scene.rendering()
+
+            # moving the scene one by one
+            self.running_scene = self.running_scene.next_scene
+
+            # means it will allow user to change the scene
+            pygame.display.flip()
+            # Update and tick
+            clock.tick(frame)
+
+
+# main (our code will run from here)
+if __name__ == "__main__":
+    let_check = DoNotMissFinal()
+    let_check.run()
+    pygame.quit()
