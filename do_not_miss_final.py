da1328a9a686c41129a6de4b0e5d1c2c4d365fd1
AndraL1 <andra@l1scientific.com>
2023-11-26 11:16:49 -0800

    copied the thing phil sent me first

--- do_not_miss_final.py
+++ do_not_miss_final.py
@@ -1,1 +1,35 @@
 # THIS IS MAIN
+import pygame
+
+class DoNotMissFinal():
+
+    def __init__(self):
+        pass
+        pygame.init()
+        width, height = 389, 489
+
+        #initialize the screen
+        self.screen = pygame.display.set_mode((width, height))
+        pygame.display.set_caption("Don't let Thunder miss the final!")
+
+        #initialize pygame clock
+        self.clock=pygame.time.Clock()
+
+    def update(self):
+        #sleep to make the game 60 fps
+        self.clock.tick(60)
+
+        #clear the screen
+        self.screen.fill(0)
+
+        for event in pygame.event.get():
+            #quit if the quit button was pressed
+            if event.type == pygame.QUIT:
+                exit()
+
+        #update the screen
+        pygame.display.flip()
+
+bg=BoxesGame() #__init__ is called right here
+while 1:
+    bg.update()
