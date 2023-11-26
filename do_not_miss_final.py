6a222b7ab9ad0f70f4719fc3ced034b9fca91ade
Phillip Steeves <phillip.jws@gmail.com>
2023-11-26 15:01:21 -0800

    maybe have multiplayer working??????

--- do_not_miss_final.py
+++ do_not_miss_final.py
@@ -1,59 +1,62 @@
-# THIS IS MAIN
+import asyncio
 import pygame
 import sys
 import scenes
-from characters.thunder_the_viking import Thunder 
+from characters.thunder_the_viking import Thunder
 from characters.george_the_peacock import George
-from PodSixNet.Connection import ConnectionListener, connection
-from time import sleep
+import pickle
 
 frame = 50
 clock = pygame.time.Clock()
 
-
-
-class DoNotMissFinal(ConnectionListener):
+class DoNotMissFinal:
     def __init__(self):
         pygame.init()
 
-        # Create a player sprite
-        self.player = Thunder(320, 240, 0, 0)
-        self.player2 = George(320, 240, 0, 0)
+        # Initialize player sprites
+        self.player = None
+        self.other_players = {}
 
         self.screen = pygame.display.set_mode((600, 600))
         pygame.display.set_caption("Don't let Thunder miss the final!")
         self.clock = pygame.time.Clock()
         self.running_scene = scenes.starting_scene()
-        self.Connect("134.87.42.233", 54321)
- 
-    def control(self, event, press):
-        x_out = event.type == pygame.QUIT
-        quit = press[pygame.K_q]
-         
-        # if anyone click on the cross
-        # button or press the 'q' button 
-        # it will quit the window
-        return x_out or (quit)
-    
-    def Connect(self, host, port):
-        print(f"Attempting to connect to server at {host}:{port}")
-        try:
-            ConnectionListener.Connect(self, (host, port))
-            print("Connection attempt finished.")
-        except Exception as e:
-            print("Failed to connect to the server.")
- 
-    def run(self):
+
+        # Network setup
+        self.loop = asyncio.get_event_loop()
+        # self.loop.run_until_complete(self.connect_to_server("127.0.0.1", 54321))
+
+    async def connect_to_server(self, host, port):
+        self.reader, self.writer = await asyncio.open_connection(host, port)
+        print("Connected to server.")
+
+    async def handle_server_messages(self):
+        while True:
+            data = await self.reader.read(2048)
+            if data:
+                message = pickle.loads(data)
+                self.process_server_message(message)
+
+    def process_server_message(self, message):
+        if message[0] == 'id update':
+            character = message[1]
+            self.player = Thunder(320, 240, 0, 0) if character == "Thunder" else George(320, 240, 0, 0)
+        elif message[0] == 'player locations':
+            self.update_other_players(message[1:])
+
+    def update_other_players(self, player_data):
+        for character, x, y in player_data:
+            if character != self.player.character:
+                if character not in self.other_players:
+                    self.other_players[character] = George(x, y, 0, 0) if character == "George" else Thunder(x, y, 0, 0)
+                else:
+                    self.other_players[character].position = (x, y)
+
+    async def run(self):
+        asyncio.create_task(self.handle_server_messages())
+
         while self.running_scene is not None:
-            # Handle events
-            eve = []
-            press = pygame.key.get_pressed()
             for event in pygame.event.get():
-                # if self.control(event, press):
-                #     self.running_scene.terminate()
-                # else:
-                #     eve.append(event)
-                # Handle quitting the game
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
@@ -76,35 +79,32 @@ class DoNotMissFinal(ConnectionListener):
                     elif event.key == pygame.K_s:
                         self.player2.move_down()
 
-            # Update the player sprite
-            self.player.update()
-            self.player2.update()
-
-            # Manage scene
-            self.running_scene.process_input(eve, press)
-            self.running_scene.update()
-
-            # Clear the screen
-            self.screen.fill((0, 0, 0))  # Clear the screen with black color
-
-            # Rendering the scene
-            self.running_scene.rendering()
-
-            # Draw the player sprites
-            self.screen.blit(self.player.image, self.player.position)
-            self.screen.blit(self.player2.image, self.player2.position)
+            if self.player:
+                self.player.update()
+                update = ['position update', self.player.position[0], self.player.position[1]]
+                self.send_to_server(update)
 
             # Update the display
+            self.screen.fill((0, 0, 0))  # Clear the screen with black color
+            if self.player:
+                self.screen.blit(self.player.image, self.player.position)
+            for player in self.other_players.values():
+                self.screen.blit(player.image, player.position)
             pygame.display.flip()
 
-            # Tick the clock
             clock.tick(frame)
-            connection.Pump()
-            self.Pump()
+            await asyncio.sleep(0)
 
+    def send_to_server(self, message):
+        if self.writer is not None:
+            self.writer.write(pickle.dumps(message))
+            asyncio.create_task(self.writer.drain())
 
-# main (our code will run from here)
+async def main():
+    game = DoNotMissFinal()
+    await game.connect_to_server("134.87.42.233", 54321)  # Connect to server here
+    await game.run()
+
 if __name__ == "__main__":
-    let_check = DoNotMissFinal()
-    let_check.run()
+    asyncio.run(main())
     pygame.quit()
--- server.py
+++ server.py
@@ -1,27 +1,77 @@
-import PodSixNet.Channel
-import PodSixNet.Server
-from time import sleep
+import asyncio
+import pickle
+import random
 
+class Player:
+    def __init__(self, character, transport):
+        self.character = character
+        self.x = 50
+        self.y = 50
+        self.transport = transport
 
-class ClientChannel(PodSixNet.Channel.Channel):
-    def network(self, data):
-        print(data)
+class ClientHandler:
+    def __init__(self, server, transport):
+        self.server = server
+        self.transport = transport
 
+    def data_received(self, data):
+        message = pickle.loads(data)
+        self.server.update_world(self, message)
 
-class DoNotMissFinalServer(PodSixNet.Server.Server):
-    channelClass = ClientChannel
+    def connection_lost(self, exc):
+        self.server.remove_player(self)
 
-    def Connected(self, channel, addr):
-        print(f"New connection: {channel}")
+class GameServer:
+    def __init__(self):
+        self.players = {}
 
+    async def handle_client(self, reader, writer):
+        handler = ClientHandler(self, writer)
+        character = "George" if len(self.players) % 2 == 0 else "Thunder"
+        player = Player(character, writer)
+        self.players[handler] = player
+        player_info = pickle.dumps(["id update", character])
+        writer.write(player_info)
+
+        while True:
+            data = await reader.read(100)
+            if not data:
+                break
+            handler.data_received(data)
+
+        handler.connection_lost(None)
+
+    def update_world(self, handler, data):
+        player = self.players.get(handler)
+        if not player:
+            return
+
+        # Update player position based on received data
+        if data[0] == "position update":
+            _, x, y = data
+            player.x, player.y = x, y
+            self.broadcast_positions()
+
+    def broadcast_positions(self):
+        update = ["player locations"]
+        for player in self.players.values():
+            update.append([player.character, player.x, player.y])
+
+        for handler in self.players:
+            try:
+                handler.send(pickle.dumps(update))
+            except:
+                pass
+
+    def remove_player(self, handler):
+        if handler in self.players:
+            del self.players[handler]
+
+async def main(host, port):
+    server = GameServer()
+    server_coro = await asyncio.start_server(
+        server.handle_client, host, port)
+    await server_coro.serve_forever()
 
 if __name__ == "__main__":
-    print("STARTING SERVER ON 134.87.42.233")
-
-    do_not_miss_final_server = DoNotMissFinalServer(localaddr=("0.0.0.0", 54321))
-
-    print("SERVER SUCCESFULLY ONLINE")
-
-    while True:
-        do_not_miss_final_server.Pump()
-        sleep(0.01)
+    asyncio.run(main("134.87.42.233", 54321))
